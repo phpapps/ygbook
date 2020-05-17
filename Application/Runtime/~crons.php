c4df4d8b3ac816ecc989bff93a0b86ba934e2f88
netsun <303078098@qq.com>
2020-05-17 15:11:49 +0800

    no message

---
+++ Application/Runtime/~crons.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  'cron_pick' => 
+  array (
+    0 => 'autopick',
+    1 => 1800,
+    2 => 1589700753,
+  ),
+  'cron_dataarea_update' => 
+  array (
+    0 => 'dataarea_update',
+    1 => 1800,
+    2 => 1589700753,
+  ),
+);
+?>
