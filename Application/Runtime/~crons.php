11581c742b87e8b2cb84b3f3b1f2cc04277eebae
netsun <303078098@qq.com>
2020-06-11 01:28:12 +0800

    no message

--- Application/Runtime/~crons.php
+++ Application/Runtime/~crons.php
@@ -4,13 +4,13 @@ return array (
   array (
     0 => 'autopick',
     1 => 1800,
-    2 => 1591808544,
+    2 => 1591811732,
   ),
   'cron_dataarea_update' => 
   array (
     0 => 'dataarea_update',
     1 => 1800,
-    2 => 1591808544,
+    2 => 1591811732,
   ),
 );
 ?>
